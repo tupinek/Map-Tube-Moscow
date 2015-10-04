ce9a2b77ae6546c59d12d38cb75bab59f0fcf9a9
Michal Špaček <tupinek@gmail.com>
2015-10-04 10:22:57 +0200

    Add EXAMPLE4 to doc.

--- Moscow.pm
+++ Moscow.pm
@@ -183,6 +183,39 @@ For more information about Moscow Map, click L<here|https://ru.wikipedia.org/wik
 
 =end html
 
+=head1 EXAMPLE4
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Encode qw(encode_utf8);
+ use Map::Tube::Moscow;
+
+ # Object.
+ my $obj = Map::Tube::Moscow->new;
+
+ # Get lines.
+ my $lines_ar = $obj->get_lines;
+
+ # Print out.
+ map { print encode_utf8($_->name)."\n"; } sort @{$lines_ar};
+
+ # Output:
+ # Арбатско-Покровская линия
+ # Бутовская линия
+ # Замоскворецкая линия
+ # Калининско-Солнцевская линия
+ # Калужско-Рижская линия
+ # Каховская линия
+ # Кольцевая линия
+ # Люблинско-Дмитровская линия
+ # Серпуховско-Тимирязевская линия
+ # Сокольническая линия
+ # Таганско-Краснопресненская линия
+ # Филёвская линия
+
 =head1 DEPENDENCIES
 
 L<File::Share>,
