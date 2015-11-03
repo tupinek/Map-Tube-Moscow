68ca43e90d3a801339e9dd19ab48c10358b60cee
Michal Špaček <tupinek@gmail.com>
2015-11-03 09:27:13 +0100

    Add EXAMPLES section to doc.

--- Moscow.pm
+++ Moscow.pm
@@ -228,6 +228,47 @@ For more information about Moscow Map, click L<here|https://ru.wikipedia.org/wik
  # Таганско-Краснопресненская линия
  # Филёвская линия
 
+=head1 EXAMPLE5
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Encode qw(decode_utf8 encode_utf8);
+ use Map::Tube::Moscow;
+
+ # Arguments.
+ if (@ARGV < 1) {
+         print STDERR "Usage: $0 line\n";
+         exit 1;
+ }
+ my $line = decode_utf8($ARGV[0]);
+
+ # Object.
+ my $obj = Map::Tube::Moscow->new;
+
+ # Get stations for line.
+ my $stations_ar = $obj->get_stations($line);
+
+ # Print out.
+ map { print encode_utf8($_->name)."\n"; } @{$stations_ar};
+
+ # Output:
+ # Usage: __PROG__ line
+
+ # Output with 'foo' argument.
+ # Map::Tube::get_stations(): ERROR: Invalid Line Name [foo]. (status: 105) file __PROG__ on line __LINE__
+
+ # Output with 'Бутовская линия' argument.
+ # Битцевский парк
+ # Лесопарковая
+ # Улица Старокачаловская
+ # Улица Скобелевская
+ # Бульвар адмирала Ушакова
+ # Улица Горчакова
+ # Бунинская аллея
+
 =head1 DEPENDENCIES
 
 L<File::Share>,
